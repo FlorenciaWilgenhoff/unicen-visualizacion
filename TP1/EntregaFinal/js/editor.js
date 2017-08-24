8ff5346df18973eed2252e6a8b70ce0e6ca47658
Florencia <florenciawilgenhoff12@gmail.com>
2017-08-24 10:31:32 -0300

    Rediseño + Funcionalidad en los botones

--- TP1/EntregaFinal/js/editor.js
+++ TP1/EntregaFinal/js/editor.js
@@ -1,111 +1,249 @@
-//para guardar imagen es: cxt1.save();
-function getRed(imageData, x, y){
-  index = (x + y * imageData.width) * 4;
-  return imageData.data[index + 0];
- }
+//hacer un slider
+//terminar los ultimos 3 filtros
+//restaurar imagen a la original
+//seleccionar imagen 
+let canvas = document.getElementById("canvas");
 
-  function getGreen(imageData, x, y){
-  index = (x + y * imageData.width) * 4;
-  return imageData.data[index + 1];
- }
+let ctx = canvas.getContext("2d");
+var width= canvas.width;
+var height= canvas.height ;
 
-  function getBlue(imageData, x, y){
-  index = (x + y * imageData.width) * 4;
-  return imageData.data[index + 2];
- }
- function greyScale(imageData){
-   index = (x + y * imageData.width) * 4;
-    var BlackAndWhite = (getRed(imageData, x, y)+ getGreen(imageData, x , y)+ getBlue(imageData, x , y))/3;
+var imageData;
+
+var image1 = new Image();
+image1.src = "panda.jpg";
+
+image1.onload = function(){
+  canvas.width =this.width;
+  canvas.height =this.height;
+  ctx.drawImage(this, 0, 0);
+  imageData = ctx.getImageData(0, 0, this.width, this.height);
+
+}
+function changeImage(imageData){
+  ctx.drawImage(image1, 0, 0);
+  ctx.putImageData(imageData, 0,0);
+
+}
+btnGuardar();
+function btnGuardar(){
+  var save = document.getElementById("guardarImagen");
+  save.addEventListener("click", function(){
+    canvas.toDataURL("images/jpg");
+    console.log("hola");
+  });
+}
+
+function btnSeleccionar(){
+  var select = document.getElementById("selectImage");
+  select.addEventListener("click", function(){
+    //codigo
+  });
+}
+
+function btnRestaurar(){
+  var restore = document.getElementById("restaurarImagen");
+  restore.addEventListener("click", function(){
+  ctx.remove();
+  });
+}
+
+function greyScale(){
+  for ( x = 0; x < canvas.width; x++) {
+   for ( y = 0; y < canvas.height; y++) {
+     index = (x + y * imageData.width) * 4;
+     var BlackAndWhite = (getRed(imageData, x, y)+ getGreen(imageData, x , y)+ getBlue(imageData, x , y))/3;
      imageData.data[index+0]=BlackAndWhite;
      imageData.data[index+1]=BlackAndWhite;
      imageData.data[index+2]=BlackAndWhite;
      imageData.data[index+3]=255;
+
+   }
  }
- function negativo(imageData){
-   index = (x + y * imageData.width) * 4;
+
+ changeImage(imageData);
+}
+
+btnGreyScale();
+
+function btnGreyScale(){
+  var escalaGris = document.getElementById("greyScale");
+  escalaGris.addEventListener("click", function(){
+    greyScale();
+  });
+}
+
+function negativo(){
+  for ( x = 0; x < canvas.width; x++) {
+   for ( y = 0; y < canvas.height; y++) {
+     index = (x + y * imageData.width) * 4;
      imageData.data[index+0]=255-getRed(imageData, x, y);
      imageData.data[index+1]=255-getGreen(imageData, x, y);
      imageData.data[index+2]=255-getBlue(imageData, x, y);
      imageData.data[index+3]=255;
+   }
  }
- function sepia(imageData){
-  index = (x + y * imageData.width) * 4;
+
+ changeImage(imageData);
+
+}
+btnNegativo();
+function btnNegativo(){
+  var negative = document.getElementById("negativo");
+  negative.addEventListener("click", function(){
+    negativo();
+  });
+}
+
+function sepia(){
+  for ( x = 0; x < canvas.width; x++) {
+   for ( y = 0; y < canvas.height; y++) {
+    index = (x + y * imageData.width) * 4;
     var calculoRed= (getRed(imageData, x, y) * 0.393) + (getGreen(imageData, x, y) * 0.769) + (getBlue(imageData, x, y) * 0.189)
     var calculoGreen= (getRed(imageData, x, y) * 0.349) + (getGreen(imageData, x, y) * 0.686) + (getBlue(imageData, x, y) * 0.168)
     var calculoBlue=(getRed(imageData, x, y) * 0.272) + (getGreen(imageData, x, y) * 0.534) + (getBlue(imageData, x, y) * 0.131)
-     imageData.data[index+0]=calculoRed+30;
-     imageData.data[index+1]=calculoGreen+25;
-     imageData.data[index+2]=calculoBlue;
-     imageData.data[index+3]=255;
- }
- function binarizacion (imageData){
-  var binarizacion = (getRed(imageData, x, y) + getGreen(imageData, x, y) + getBlue(imageData, x, y)) / 3;
-            if (binarizacion > 130) {
-             index = (x + y * imageData.width) * 4;
-             imageData.data[index+0]=0;
-             imageData.data[index+1]=0;
-             imageData.data[index+2]=0;
-             imageData.data[index+3]=255;
-                    }
-            else {
-                index = (x + y * imageData.width) * 4;
-                imageData.data[index+0]=255;
-                imageData.data[index+1]=255;
-                imageData.data[index+2]=255;
-                imageData.data[index+3]=255;
-            }
-            
- }
- function brillo(imageData){
-  index = (x + y * imageData.width) * 4;
-     imageData.data[index+0]=1.35*getRed(imageData, x, y);
-     imageData.data[index+1]=1.35*getGreen(imageData, x, y);
-     imageData.data[index+2]=1.35*getBlue(imageData, x, y);
-     imageData.data[index+3]=255;
- }
- function saturacion (imageData){
+    imageData.data[index+0]=calculoRed+30;
+    imageData.data[index+1]=calculoGreen+25;
+    imageData.data[index+2]=calculoBlue;
+    imageData.data[index+3]=255;
+  }
+}
 
+changeImage(imageData);
+}
+btnSepia();
+function btnSepia(){
+  var Sepia = document.getElementById("sepia");
+  Sepia.addEventListener("click", function(){
+    sepia();
+  });
+}
+function binarizacion (){
+  for ( x = 0; x < canvas.width; x++) {
+   for ( y = 0; y < canvas.height; y++) {
+    var binarizacion =110;
+    var v = (0.2126 * getRed(imageData, x, y) + 0.7152 * getGreen(imageData, x, y) + 0.0722 * getBlue(imageData, x, y) >= binarizacion) ? 255 : 0;
+    index = (x + y * imageData.width) * 4;
+    imageData.data[index+0]=v;
+    imageData.data[index+1]=v;
+    imageData.data[index+2]=v;
+    imageData.data[index+3]=255;
+  }
+}
+
+changeImage(imageData);
+
+}
+
+function btnBinarizacion(){
+  var binary = document.getElementById("binarizacion");
+  binary.addEventListener("click", function(){
+    binarizacion();
+  });
+}
+btnBinarizacion();
+function brillo(){
+  for ( x = 0; x < canvas.width; x++) {
+   for ( y = 0; y < canvas.height; y++) {
+    index = (x + y * imageData.width) * 4;
+    imageData.data[index+0]=1.35*getRed(imageData, x, y);
+    imageData.data[index+1]=1.35*getGreen(imageData, x, y);
+    imageData.data[index+2]=1.35*getBlue(imageData, x, y);
+    imageData.data[index+3]=255;
+  }
+}
+
+changeImage(imageData);
+}
+btnBrillo();
+function btnBrillo(){
+  var brightness = document.getElementById("brillo");
+  brightness.addEventListener("click", function(){
+    brillo();
+  });
+}
+
+function saturacion (){
+  for ( x = 0; x < canvas.width; x++) {
+   for ( y = 0; y < canvas.height; y++) {
      index = (x + y * imageData.width) * 4;
      var saturationValue = 2; 
-      var luzRed = 0.3086;
-      var luzGreen = 0.6094;
-      var luzBlue = 0.0820;
-      var az = (1 - saturationValue)*luzRed + saturationValue;
-      var bz = (1 - saturationValue)*luzGreen;
-      var cz = (1 - saturationValue)*luzBlue;
-      var dz = (1 - saturationValue)*luzRed;
-      var ez = (1 - saturationValue)*luzGreen + saturationValue;
-      var fz = (1 - saturationValue)*luzBlue;
-      var gz = (1 - saturationValue)*luzRed;
-      var hz = (1 - saturationValue)*luzGreen;
-      var iz = (1 - saturationValue)*luzBlue + saturationValue;
-      var saturadoRed = (az * getRed(imageData, x, y) + bz * getGreen(imageData, x, y) + cz * getBlue(imageData, x, y)); 
-      var saturadoGreen = (dz * getRed(imageData, x, y) + ez * getGreen(imageData, x, y) + fz * getBlue(imageData, x, y)); 
-      var saturadodBlue = (gz * getRed(imageData, x, y)+ hz * getGreen(imageData, x, y) + iz * getBlue(imageData, x, y)); 
+     var luzRed = 0.3086;
+     var luzGreen = 0.6094;
+     var luzBlue = 0.0820;
+     var az = (1 - saturationValue)*luzRed + saturationValue;
+     var bz = (1 - saturationValue)*luzGreen;
+     var cz = (1 - saturationValue)*luzBlue;
+     var dz = (1 - saturationValue)*luzRed;
+     var ez = (1 - saturationValue)*luzGreen + saturationValue;
+     var fz = (1 - saturationValue)*luzBlue;
+     var gz = (1 - saturationValue)*luzRed;
+     var hz = (1 - saturationValue)*luzGreen;
+     var iz = (1 - saturationValue)*luzBlue + saturationValue;
+     var saturadoRed = (az * getRed(imageData, x, y) + bz * getGreen(imageData, x, y) + cz * getBlue(imageData, x, y)); 
+     var saturadoGreen = (dz * getRed(imageData, x, y) + ez * getGreen(imageData, x, y) + fz * getBlue(imageData, x, y)); 
+     var saturadodBlue = (gz * getRed(imageData, x, y)+ hz * getGreen(imageData, x, y) + iz * getBlue(imageData, x, y)); 
      imageData.data[index+0]=saturadoRed;
      imageData.data[index+1]=saturadoGreen;
      imageData.data[index+2]=saturadodBlue;
      imageData.data[index+3]=255;
-
+   }
  }
- function suavizado(imageData){
 
- }
- function detectorBordes(imageData){
+ changeImage(imageData);
 
- }
- function blur(imageData){
+}
+btnSaturacion();
+function btnSaturacion(){
+  var saturation = document.getElementById("saturacion");
+  saturation.addEventListener("click", function(){
+    saturacion();
+  });
+}
 
- }
- function contraste(imageData){
-  var contraste=100;
+function suavizado(){
+
+}
+function detectorBordes(){
+
+}
+function blur(){
+
+}
+function contraste(){
+  for ( x = 0; x < canvas.width; x++) {
+   for ( y = 0; y < canvas.height; y++) {
+    var contraste=100;
     var factor = (259 * (contraste + 255)) / (255 * (259 - contraste));
-     index = (x + y * imageData.width) * 4;
-     imageData.data[index+0]=factor*(getRed(imageData, x, y)-128)+128;
-     imageData.data[index+1]=factor*(getGreen(imageData, x, y)-128)+128;
-     imageData.data[index+2]=factor*(getBlue(imageData, x, y)-128)+128;
-     imageData.data[index+3]=255;
- }
- //funcion espejo
- //ctx.translate(2*this.height,0);
-//ctx.scale(-1,1);
+    index = (x + y * imageData.width) * 4;
+    imageData.data[index+0]=factor*(getRed(imageData, x, y)-128)+128;
+    imageData.data[index+1]=factor*(getGreen(imageData, x, y)-128)+128;
+    imageData.data[index+2]=factor*(getBlue(imageData, x, y)-128)+128;
+    imageData.data[index+3]=255;
+  }
+}
+
+changeImage(imageData);
+}
+
+btnContraste();
+function btnContraste(){
+  var contrast = document.getElementById("contraste");
+  contrast.addEventListener("click", function(){
+    contraste();
+  });
+}
+function getRed(imageData, x, y){
+  index = (x + y * imageData.width) * 4;
+  return imageData.data[index + 0];
+}
+
+function getGreen(imageData, x, y){
+  index = (x + y * imageData.width) * 4;
+  return imageData.data[index + 1];
+}
+
+function getBlue(imageData, x, y){
+  index = (x + y * imageData.width) * 4;
+  return imageData.data[index + 2];
+}
